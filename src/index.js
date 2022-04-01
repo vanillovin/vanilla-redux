92dae197e2f2b33a3e3f05774bde382d5d701b2d
vanillovin <vanillovin@gmail.com>
2022-04-01 22:36:12 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
+import { createStore } from "redux";
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+// data를 modify하는 함수! 여기가 유일하게 data를 바꿀 수 있는 곳
+const countModifier = (state = 0) => {
+  // .... modify state
 
-number.innerText = count;
+  // return 값이 우리의 application에 있는 data가 됨
+  return state;
+};
 
-const updateText = () => {
-  number.innerText = count;
-}
+// store는 data(state)를 저장하는 장소. state는 너의 app에서 바뀌는 data
+// store를 만들면 우리는 countModifier를 initial state로 불러옴
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-}
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-}
+console.log(countStore);
+// {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
+// 0
