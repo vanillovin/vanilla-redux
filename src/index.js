9161cb9692342aebddf35ecce70f1b1394f96310
vanillovin <vanillovin@gmail.com>
2022-04-02 13:38:29 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux';
 
+const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
+number.innerText = 0;
 
 const countModifier = (count = 0, action) => {
   console.log(count, action);
@@ -16,11 +18,18 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'MINUS' }); 
+const onChange = () => {
+  console.log('onChange', countStore.getState());
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState()); // 4
+// store 안에 있는 변화 감지
+// subscribe는 우리에게 store 안에 있는 변화들을 알 수 있게 해줌
+countStore.subscribe(onChange); // store 안의 변화를 감지하면 onChange 실행
+
+const handleAdd = () => countStore.dispatch({ type: "ADD" });
+
+const handleMinus = () => countStore.dispatch({ type: "MINUS" });
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
