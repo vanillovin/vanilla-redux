cafe367422322593825aedcebeb8ba22f2e1d49d
vanillovin <vanillovin@gmail.com>
2022-04-02 11:48:48 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-// data를 modify하는 함수! 여기가 유일하게 data를 바꿀 수 있는 곳
-const countModifier = (state = 0) => {
-  // .... modify state
-
-  // return 값이 우리의 application에 있는 data가 됨
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-// store는 data(state)를 저장하는 장소. state는 너의 app에서 바뀌는 data
-// store를 만들면 우리는 countModifier를 initial state로 불러옴
 const countStore = createStore(countModifier);
 
-console.log(countStore);
-// {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' }); 
 
-console.log(countStore.getState());
-// 0
+console.log(countStore.getState()); // 4
