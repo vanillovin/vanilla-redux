63b8c650bc94035e90207f5108f66ce104817c23
vanillovin <vanillovin@gmail.com>
2022-04-02 21:54:41 +0900

    #3.0 Setup

--- src/index.js
+++ src/index.js
@@ -1,75 +1,10 @@
-import { createStore } from 'redux';
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './components/App';
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+// https://github.com/reactwg/react-18/discussions/5
+const container = document.getElementById('root');
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+const root = ReactDOMClient.createRoot(container);
 
-const addToDo = text => {
-  return {
-    type: ADD_TODO,
-    text
-  };
-};
-
-const deleteToDo = id => {
-  return {
-    type: DELETE_TODO,
-    id
-  };
-};
-
-const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case ADD_TODO:
-      const newToDoObj = { id: Date.now(), text: action.text };
-      return [newToDoObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter(toDo => toDo.id !== action.id);
-      return cleaned;
-    default:
-      return state;
-  }
-};
- 
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const dispatchAddToDo = text => {
-  store.dispatch(addToDo(text));
-};
-
-const dispatchDeleteToDo = e => {
-  const id = +e.target.parentNode.id;
-  store.dispatch(deleteToDo(id));
-};
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDos);
-
-const onSubmit = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+root.render(<App />);
