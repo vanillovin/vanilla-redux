cd862588e59746c051e8c715bdc825a6c0ec9a3a
vanillovin <vanillovin@gmail.com>
2022-04-02 15:31:50 +0900

    #2.1 State Mutation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-// never! mutate state
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
@@ -7,11 +7,15 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// never! mutate state
+// store를 수정할 수 있는 유일한 방법은 action을 보내는 것!
+// **mutate 하는 게 아니라 새로운 state를 리턴**
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      // X state.push(action.text) X
+      return [...state, { id: Date.now(), text: action.text }];
     case DELETE_TODO:
       return [];
     default:
@@ -21,18 +25,12 @@ const reducer = (state = [], action) => {
  
 const store = createStore(reducer);
 
-// HTML을 수정해 주는 것뿐 우리 app은 dataless
-const createToDo = toDo => {
-  const li = document.querySelector('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
-}
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
   store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
