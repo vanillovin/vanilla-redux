6ad609c0efe6126a82999288d05fd02aa5df8da5
vanillovin <vanillovin@gmail.com>
2022-04-02 14:00:57 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -5,31 +5,32 @@ const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 const countStore = createStore(countModifier);
 
 const onChange = () => {
-  console.log('onChange', countStore.getState());
   number.innerText = countStore.getState();
 };
 
-// store 안에 있는 변화 감지
-// subscribe는 우리에게 store 안에 있는 변화들을 알 수 있게 해줌
 countStore.subscribe(onChange); // store 안의 변화를 감지하면 onChange 실행
 
-const handleAdd = () => countStore.dispatch({ type: "ADD" });
+// action은 modifier와 communicate 하는 방법으로 object여야 함
+const handleAdd = () => countStore.dispatch({ type: ADD });
 
-const handleMinus = () => countStore.dispatch({ type: "MINUS" });
+const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
