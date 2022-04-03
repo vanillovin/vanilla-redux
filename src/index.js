a61678d4b087314cb58e7257a4e48579d34f7575
vanillovin <vanillovin@gmail.com>
2022-04-03 21:31:10 +0900

    #3.1 Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
 
 // https://github.com/reactwg/react-18/discussions/5
 const container = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(container);
-
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
