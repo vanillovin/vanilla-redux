873930f4093c057c600d2dcb6ce8f3acd62966fb
vanillovin <vanillovin@gmail.com>
2022-04-04 21:59:11 +0900

    #3.5 Detail Screen

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from 'react-router-dom';
 
-function Detail() {
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const toDo = toDos.find(toDo => toDo.id === +id);
+  const date = new Date(toDo?.id);
   return (
-    <div>Detail</div>
-  )
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {`
+        ${date.toISOString().split('T')[0]
+        } ${date.toISOString().split('T')[1].substring(0, 5)}
+      `}</h5>
+    </>
+  );
 }
 
-export default Detail
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
